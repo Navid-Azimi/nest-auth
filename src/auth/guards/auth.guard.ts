5c11f5a10c1712c8b853dba540950758a76c1a66
Sayed Navid <navidjan.azimi132@gmail.com>
2025-04-26 14:48:41 +0430

    auth without passport is done

---
+++ src/auth/guards/auth.guard.ts
@@ -0,0 +1,33 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private readonly jwtservice: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const authorization = request.headers.authorization;
+    const token = authorization?.split(' ')[1];
+
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
+    try {
+      const tokenPayload = await this.jwtservice.verifyAsync(token);
+      request.user = {
+        userId: tokenPayload.sub,
+        username: tokenPayload.username,
+      };
+      return true;
+    } catch (e) {
+      throw new UnauthorizedException(e);
+    }
+  }
+}
