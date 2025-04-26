b1f8070ea9d8f7b84e56830aa2e25d7d52d6998a
Sayed Navid <navidjan.azimi132@gmail.com>
2025-04-26 13:37:36 +0430

    users module and service are done

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
 
+export type User = {
+  userId: number;
+  username: string;
+  password: string;
+};
+
+const users: User[] = [
+  {
+    userId: 1,
+    password: 'Hellowrold',
+    username: 'navid',
+  },
+  {
+    userId: 2,
+    password: 'Dontknow',
+    username: 'Alice',
+  },
+];
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  async findUserByName(username: string): Promise<User | undefined> {
+    return users.find((user) => user.username === username);
+  }
+}
